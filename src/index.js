6dfbcb063cc14a92f68444eca7e42f82c48dfeaa
Richu98 <richuthomas98@gmail.com>
2020-05-25 13:31:22 +0300

    feat: Add redux-persist and persist currentUser (#10)

--- src/index.js
+++ src/index.js
@@ -2,16 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import reducers from './reducers'
-import middleware from './middleware'
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import storage from "redux-persist/lib/storage";
+
+import reducers from "./reducers";
+import middleware from "./middleware";
 import App from "./components/App";
 
-const store = createStore(reducers, middleware);
+const persistConfig = {
+  key: "currentUser",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = createStore(persistedReducer, middleware);
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<h3>Loading...</h3>} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
